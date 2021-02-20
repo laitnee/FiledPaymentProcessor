ca36af2b0a6386ee29f818d94fc56de2c63d0d8d
Idowu Hayub <idowuolatunji3@gmail.com>
2021-02-20 07:32:35 +0100

    Initial Project files

---
+++ Filed.PaymentProcessor.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Filed.PaymentProcessor.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ FiledPaymentProcessor.Data/PaymentProcessorContext.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FiledPaymentProcessor.Data
+{
+    public class Class1
+    {
+    }
+}
