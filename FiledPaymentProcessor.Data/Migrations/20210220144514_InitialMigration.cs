8ac5f75eba841ffae3d4d5356cfa618b7c4ba7ac
Idowu Hayub <idowuolatunji3@gmail.com>
2021-02-22 00:22:43 +0100

    Added Migration Files

---
+++ FiledPaymentProcessor.Data/Migrations/20210220144514_InitialMigration.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FiledPaymentProcessor.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreditCardNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CardHolder = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SecurityCode = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    PaymentDetailId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentStatuses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PaymentStatuses_PaymentDetails_PaymentDetailId",
+                        column: x => x.PaymentDetailId,
+                        principalTable: "PaymentDetails",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentStatuses_PaymentDetailId",
+                table: "PaymentStatuses",
+                column: "PaymentDetailId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentStatuses");
+
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
